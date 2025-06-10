c4bbb1ba73dd24999f1f4697afc85e2f72962507
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-06-10 18:17:28 +0200

    fixed heart button style

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import { useCoffee } from '../contexts/CoffeeContext';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
 
 const FavoriteButton = ({ coffee, onFavorite }) => {
     const { favorites } = useCoffee();
 
     return (
-        <button onClick={() => onFavorite(coffee)}>
+        <button
+            onClick={() => onFavorite(coffee)}
+            className='heart-btn'
+        >
             {favorites.includes(coffee.id) ? (
                 <AiFillHeart color="red" size={24} />
             ) : (
-                <AiOutlineHeart size={24} />
+                <AiOutlineHeart color="gray" size={24} />
             )}
         </button>
+
     );
 };
 
