981518ae13b30f868c25183463dd83c533111e7c
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-06-11 15:42:01 +0200

    added file types and database from backend

---
+++ backend/types.ts
@@ -0,0 +1,11 @@
+export type Coffee = {
+    title: string;
+    category: string;
+    roaster: string;
+    origin: string;
+    process: string;
+    roastLevel: string;
+    grams: number;
+    price: number;
+    rating: number;
+};
